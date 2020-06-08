f718264227a7878654a7998811729e2386f58c6c
andres-vasquuez-coderoad-com <andres.vasquez@mojix.com>
2020-06-08 07:56:21 -0400

    Clase 08-06 Lifecycle

--- app/src/main/java/com/andresvasquez/holamundoenclases/LoginActivity.java
+++ app/src/main/java/com/andresvasquez/holamundoenclases/LoginActivity.java
@@ -3,12 +3,54 @@ package com.andresvasquez.holamundoenclases;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static String LOG = LoginActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Log.e(LOG, "onCreate");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(LOG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(LOG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(LOG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(LOG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e(LOG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(LOG, "onDestroy");
+    }
+
+
 }
