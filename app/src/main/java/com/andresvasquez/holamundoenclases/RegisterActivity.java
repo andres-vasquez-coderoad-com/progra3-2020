acfa96243d509426ad774e4a4bc1c1c4fbcf0ff7
andres-vasquuez-coderoad-com <andres.vasquez@mojix.com>
2020-06-12 09:08:40 -0400

    Clase 10-06 Activity programmatically + form validation.

--- app/src/main/java/com/andresvasquez/holamundoenclases/RegisterActivity.java
+++ app/src/main/java/com/andresvasquez/holamundoenclases/RegisterActivity.java
@@ -2,15 +2,21 @@ package com.andresvasquez.holamundoenclases;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.andresvasquez.holamundoenclases.model.User;
 import com.andresvasquez.holamundoenclases.utils.DimensionUtils;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -33,6 +39,9 @@ public class RegisterActivity extends AppCompatActivity {
 
     private TextView countryTextview;
     private EditText countryEditText;
+
+    private TextView genderTextview;
+    private RadioGroup genderRadioGroup;
 
     private LinearLayout buttonsLinearLayout;
     private Button sendButton;
@@ -75,6 +84,7 @@ public class RegisterActivity extends AppCompatActivity {
         parentLinearLayout.addView(passwordTextview);
         //Campo
         passwordEditText = new EditText(context);
+        passwordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
         parentLinearLayout.addView(passwordEditText);
 
         //Label
@@ -99,8 +109,25 @@ public class RegisterActivity extends AppCompatActivity {
         parentLinearLayout.addView(phoneTextview);
         //Campo
         phoneEditText = new EditText(context);
+        phoneEditText.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_SIGNED);
         parentLinearLayout.addView(phoneEditText);
 
+        //Label
+        genderTextview = new TextView(context);
+        genderTextview.setText("Gender");
+        //Campo
+        genderRadioGroup = new RadioGroup(context);
+
+        //Llenar las opciones
+        String[] genders = new String[]{"Male", "Female", "Not specified", "Other"};
+        for (int i = 0; i < genders.length; i++) {
+            RadioButton option = new RadioButton(context);
+            option.setId(i);
+            option.setText(genders[i]);
+            genderRadioGroup.addView(option);
+        }
+        genderRadioGroup.check(0);
+        parentLinearLayout.addView(genderRadioGroup);
 
         //Botones
         buttonsLinearLayout = new LinearLayout(context);
@@ -122,12 +149,12 @@ public class RegisterActivity extends AppCompatActivity {
                 50);//Peso
 
         sendButton = new Button(context);
-        sendButton.setText("Send");
+        sendButton.setText(getString(R.string.send));
         sendButton.setLayoutParams(buttonsLayoutParams);
         buttonsLinearLayout.addView(sendButton);
 
         cleanButton = new Button(context);
-        cleanButton.setText("Reset");
+        cleanButton.setText(getString(R.string.reset));
         cleanButton.setLayoutParams(buttonsLayoutParams);
         buttonsLinearLayout.addView(cleanButton);
 
@@ -139,7 +166,68 @@ public class RegisterActivity extends AppCompatActivity {
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String username = usernameEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+                String name = nameEditText.getText().toString().trim();
+                String country = countryEditText.getText().toString().trim();
+                String phone = phoneEditText.getText().toString().trim();
 
+                if (username.isEmpty()) {
+                    Toast.makeText(context,
+                            getString(R.string.error_empty_variable, "Username"),
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.isEmpty()) {
+                    Toast.makeText(context,
+                            getString(R.string.error_empty_variable, "Password"),
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (name.isEmpty()) {
+                    Toast.makeText(context,
+                            getString(R.string.error_empty_variable, "Name"),
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (country.isEmpty()) {
+                    Toast.makeText(context,
+                            getString(R.string.error_empty_variable, "Country"),
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (phone.isEmpty()) {
+                    Toast.makeText(context,
+                            getString(R.string.error_empty_variable, "Phone"),
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (!(phone.length() == 8 && !phone.contains(" "))) {
+                    Toast.makeText(context,
+                            getString(R.string.error_invalid_phone),
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                int phoneNumber = 0;
+                try {
+                    phoneNumber = Integer.parseInt(phone);
+                } catch (Exception ex) {
+                    Toast.makeText(context,
+                            getString(R.string.error_empty_variable),
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                User objUser = new User(name, username, password, country);
+                objUser.setPhoneNumber(phoneNumber);
+                finish();
             }
         });
 
--- app/src/main/java/com/andresvasquez/holamundoenclases/repository/UserRepository.java
+++ app/src/main/java/com/andresvasquez/holamundoenclases/repository/UserRepository.java
@@ -2,7 +2,11 @@ package com.andresvasquez.holamundoenclases.repository;
 
 import com.andresvasquez.holamundoenclases.model.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserRepository {
+    private List<User> users = new ArrayList<>();
 
     public User login(String username, String password) {
         if (username.equals("admin") && password.equals("123")) {
